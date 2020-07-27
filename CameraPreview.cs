5c765458a8f9292069a4909f054cd11cac9a4fb0
thazin <thazinoo1988@gmail.com>
2020-07-27 13:54:14 +0630

    Camera Preview
    object can seen by camera

---
+++ CameraPreview.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+// class for the object seen by camera
+public class CameraPreview : MonoBehaviour
+{
+    public GameObject obj;
+    public Camera cam;
+  
+    void Update()
+    {
+        if (obj.GetComponent<MeshRenderer>().IsVisibleFrom(cam)){
+            print("Visible");
+        }
+        else {
+            print("Not Visible");
+        }       
+    }   
+}
+ 
+public static class RendererExtensions
+{
+	public static bool IsVisibleFrom(this Renderer renderer, Camera camera)
+	{
+		Plane[] planes = GeometryUtility.CalculateFrustumPlanes(camera);
+		return GeometryUtility.TestPlanesAABB(planes, renderer.bounds);
+	}
+}
